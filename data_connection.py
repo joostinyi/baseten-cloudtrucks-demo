e6a521b131898a19314cea685c480ccc2e085b6c
joostinyi <63941848+joostinyi@users.noreply.github.com>
2022-12-01 14:10:05 -0800

    Update data_connection.py

--- data_connection.py
+++ data_connection.py
@@ -1,9 +1,6 @@
-def query_transform_data(block_input: dict, client) -> dict:
+def enrich_model_input(data: dict, snowflake_client) -> dict:
     query_result = []
-    limit = 1
-    if block_input and 'limit' in block_input:
-        limit = block_input['limit']
-    for rank, city, state in client.cursor().execute(f"SELECT RANK, CITY, STATE FROM COMPANIES LIMIT {limit}"):
+    for rank, city, state in snowflake_client.cursor().execute(f"SELECT RANK, CITY, STATE FROM COMPANIES LIMIT 5"):
         city = city.upper()
         rank += " rank"
         query_result.append([rank, city, state])
